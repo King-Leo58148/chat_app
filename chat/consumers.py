33de525a8a79fe952190391c0f59c86d32e6c84f
Louis Sanyenni <lsanyenni@gmail.com>
2026-05-22 22:06:49 +0000

    basic chat

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,29 +1,50 @@
 import json
-from sqlite3.dbapi2 import Timestamp
 from channels.generic.websocket import AsyncWebsocketConsumer
 from . models import Messages, Room
 class ChatConsumer(AsyncWebsocketConsumer):
    async def connect(self):
-    await self.accept()
+      self.room_name = self.scope['url_route']['kwargs']['room_name']
+      try: 
+         await Room.objects.aget(name=self.room_name)
+      except Room.DoesNotExist:
+         await self.accept()
+         await self.send(text_data=json.dumps
+                        ({"error":"Room does not exist"}))
+         await self.close()
+         return 
+      await self.channel_layer.group_add(
+            self.room_name,
+            self.channel_name
+            )
+      await self.accept()
 
-   
-   async def disconnect(self, close_code):
-       pass
-    
+   async def disconnect(self,close_code):
+     await self.channel_layer.group_discard(
+       self.room_name,
+       self.channel_name
+     )
+      
+
    async def receive(self,text_data):
      data = json.loads(text_data)
      
-     room, _ = await Room.objects.aget_or_create(name=data['room'])
+     room = await Room.objects.aget(name=self.room_name)
      await Messages.objects.acreate(content=data['content'],
                                     username = data['username'],
                                     reaction = data.get('reaction',None),
                                     room = room
                                     )
 
-     await self.send(text_data = json.dumps(data))
-
-
-
+     await self.channel_layer.group_send(self.room_name,{'type': 'chat_message', 
+                                                           'username':data['username'],
+                                                           'content':data['content'],
+                                    'reaction':data.get('reaction',None),
+                                 }) 
+     
+   async def chat_message(self,event):
+     event = json.dumps(event)
+     await self.send(text_data=event)
+     
     
 
 
