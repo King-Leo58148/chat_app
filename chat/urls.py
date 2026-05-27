3b5e1db15afa91acd6a78bcd2ef3d1dced750b80
Louis Sanyenni <lsanyenni@gmail.com>
2026-05-27 21:50:03 +0000

    m

--- chat/urls.py
+++ chat/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import CreateRoomView, count_active_users
 urlpatterns = [
-    ##
+    
     path('create_room/',CreateRoomView.as_view(),name='create-room'),
     path('count_active_users/<str:room_name>/',count_active_users,name='count-active-users')
   
